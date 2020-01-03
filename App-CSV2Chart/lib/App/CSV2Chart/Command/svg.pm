95aae935e33fe87946b0f623ce082c15773f4cc4
Shlomi Fish <shlomif@shlomifish.org>
2020-01-03 14:10:11 +0200

    add svg

---
+++ App-CSV2Chart/lib/App/CSV2Chart/Command/svg.pm
@@ -0,0 +1,87 @@
+package App::CSV2Chart::Command::svg;
+
+use strict;
+use warnings;
+
+use App::CSV2Chart -command;
+use App::CSV2Chart::API::ToXLSX ();
+
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+sub description
+{
+    return "Generate .svg";
+}
+
+sub abstract
+{
+    return shift->description();
+}
+
+sub opt_spec
+{
+    return (
+        [ "output|o=s", "Output path" ],
+        [ "title=s",    "Chart Title" ],
+        [ 'exec|e=s@',  "Execute command on the output" ]
+    );
+}
+
+sub execute
+{
+    my ( $self, $opt, $args ) = @_;
+
+    my $output_fn = $opt->{output};
+    my $exe       = $opt->{exec} // [];
+
+    my $fh = \*STDIN;
+
+    my $dir = tempdir();
+
+    if ( "$dir" =~ /%/ )
+    {
+        die "Invalid dir path $dir contains percent signs";
+    }
+
+    my $xlsx = $dir->child("temp.xlsx");
+
+    App::CSV2Chart::API::ToXLSX::csv_to_xlsx(
+        {
+            input_fh  => $fh,
+            output_fn => "$xlsx",
+            title     => $opt->{title},
+        }
+    );
+
+    my $svg_fn = $dir->child("out%.svg");
+    if (
+        system(
+            "ssconvert", "--export-graphs",
+            "$xlsx", ( "$svg_fn" =~ s/%/%%/r )
+        )
+        )
+    {
+        die "ssconvert failed";
+    }
+
+    # Use ->copy instead of ->move because rename does
+    # not handle cross-partition moves.
+    $svg_fn->copy($output_fn);
+
+    if (@$exe)
+    {
+        system( @$exe, $output_fn );
+    }
+
+    return;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+csv2chart xlsx - generate an .xlsx file with an embedded chart from CSV data
+
+=cut
